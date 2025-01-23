bddf0ad8d9e70457a6df355aaa53e2a3ccf92e6b
Sienna Mader <madersienna@gmail.com>
2025-01-22 19:57:50 -0500

    Working in git-lab-2

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sienna Mader
+ *        Date: 1/22/25
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl; /*add code*/
+    return 0;
+}// main
